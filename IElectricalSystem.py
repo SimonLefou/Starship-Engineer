bebe19813d15cd1935b2ff2406b6d305c54d2a7a
SimonLefou <simonlefou@protonmail.com>
2022-05-26 10:25:11 +0200

    alpha

--- IElectricalSystem.py
+++ IElectricalSystem.py
@@ -1,8 +1,24 @@
 from abc import *
+from multiprocessing.sharedctypes import Value
 
 
 class IElectricalSystem(ABC):
+    def __init__(self, n):
+        self.wattage = n
+
     @abstractmethod
     def get_wattage(self):
         pass
 
+
+
+    # Proposition pour remplacer les methodes get_wattage
+
+    @property
+    def wattage(self):          # method getter
+        return self._wattage
+        
+
+    @wattage.setter
+    def wattage(self, value):   # method setter
+        self._wattage = value
